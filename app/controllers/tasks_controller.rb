36404b1678ac7ffcc7f338e5c8b5acfab833ec63
koudaikawaguchi <k.kwgt01@gmail.com>
2020-01-26 12:43:05 +0000

    message_params->task_params

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,13 +12,13 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(message_params)
+    @task = Task.new(task_params)
 
     if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
+      flash[:success] = 'タスクが正常に投稿されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Task が投稿されませんでした'
+      flash[:danger] = 'タスクが投稿されませんでした'
       render :new
     end
   end
